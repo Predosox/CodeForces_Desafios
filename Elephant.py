e47e9160dc8b89a7356e252ad961de70fea47fc3
Arthur Pedroso <arthuralmeidapedroso3@gmail.com>
2025-10-27 14:20:15 -0300

    Anton And Letters

--- Elephant.py
+++
@@ -1,9 +0,0 @@
-import math
-coordenada_casa_elefante = int(input(""))
-
-if coordenada_casa_elefante == 5:
-    passos = 1
-else:
-    passos = math.ceil(coordenada_casa_elefante/5)
-
-print(passos)
