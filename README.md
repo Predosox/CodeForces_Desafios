8a3eb9882142e42b039c690224da07dc1c098471
Arthur Pedroso <arthuralmeidapedroso3@gmail.com>
2025-11-07 08:45:02 -0300

    133A + Adicionando tempo de resoluções

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 - 📊 Rating: Ainda em construção 🚧
 - 🏅 Rank: Em breve... 
-- 🕒 Última vez online: `05/11/2025 08:21:27`
+- 🕒 Última vez online: `07/11/2025 08:31:12`
 - 📅 Registrado em: `16/07/2024 16:36:06`
-- ✅ Problemas únicos resolvidos: `20`
+- ✅ Problemas únicos resolvidos: `21`
---
+++ lvl_900/133A - HQ9+.py
@@ -0,0 +1,54 @@
+#A. HQ9+
+
+#HQ9+ is a joke programming language which has only four one-character instructions:
+
+#"H" prints "Hello, World!",
+#"Q" prints the source code of the program itself,
+#"9" prints the lyrics of "99 Bottles of Beer" song,
+#"+" increments the value stored in the internal accumulator.
+#Instructions "H" and "Q" are case-sensitive and must be uppercase. 
+# The characters of the program which are not instructions are ignored.
+
+#You are given a program written in HQ9+. 
+# You have to figure out whether executing this program will produce any output.
+
+#Input
+#The input will consist of a single line p which will give a program in HQ9+. 
+# String p will contain between 1 and 100 characters, inclusive. 
+# ASCII-code of each character of p will be between 33 (exclamation mark) and 126 (tilde), inclusive.
+
+#Output
+#Output "YES", if executing the program will produce any output, and "NO" otherwise.
+
+#Examples
+#InputCopy
+#Hi!
+#OutputCopy
+#YES
+#InputCopy
+#Codeforces
+#OutputCopy
+#NO
+#Note
+#In the first case the program contains only one instruction — "H", which prints "Hello, World!".
+#In the second case none of the program characters are language instructions.
+
+a = input("")
+response = 0
+
+for _ in range(len(a)):
+    match a[_]:
+        case "H":
+            response += 1
+        case "Q":
+            response +=1
+        case "9": 
+            response +=1
+        
+
+if response > 0:
+    print("YES")
+else:
+    print("NO")
+
+#Start: 08:39 // End: 08:44
